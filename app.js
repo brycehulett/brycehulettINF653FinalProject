7c038eea1323533af2d7989807e40096f40caf09
bryce hulett <bryce.hulett@hotmail.com>
2022-04-26 21:07:50 -0500

    updated cors

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ connectDB();
 
 // setup
 app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '/public')));
