fce119e021d0beb911302c450eb84e12178c62e8
bryce hulett <bryce.hulett@hotmail.com>
2022-04-26 19:57:52 -0500

    added heroku site to cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
+    'https://hulett-bryce-inf651-final.herokuapp.com/',
+    'https://dazzling-snickerdoodle-777101.netlify.app/',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
